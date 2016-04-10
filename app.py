56d35722599cb4261b86e63098cbe49ffd18d410
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-04-10 22:14:16 +1000

    Only run server if script run as __main__.

--- app.py
+++ app.py
@@ -6,10 +6,8 @@ import sys
 from wsgiref.simple_server import make_server
 from wsgi import application as hello_world
 
-port = int(os.environ.get('WARPDRIVE_HTTP_PORT', '8080'))
-
-httpd = make_server('', port, hello_world)
-
-print('Serving on port %d ...' % port, file=sys.stderr)
-
-httpd.serve_forever()
+if __name__ == '__main__':
+    port = int(os.environ.get('WARPDRIVE_HTTP_PORT', '8080'))
+    httpd = make_server('', port, hello_world)
+    print('Serving on port %d ...' % port, file=sys.stderr)
+    httpd.serve_forever()
