29bd3251c737fcfc420df96532fb7db8810b7375
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-04-10 20:23:10 +1000

    Switch to print function and log to stderr.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from __future__ import print_function
+
 import os
+import sys
 
 from wsgiref.simple_server import make_server
 from wsgi import application
@@ -6,5 +9,7 @@ from wsgi import application
 port = int(os.environ.get('WARPDRIVE_HTTP_PORT', '8080'))
 
 httpd = make_server('', port, application)
-print 'Serving on port %d ...' % port
+
+print('Serving on port %d ...' % port, file=sys.stderr)
+
 httpd.serve_forever()
